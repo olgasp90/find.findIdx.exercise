61430a067b4605a6151fb03c390aabbe93af412e
Olga <olga_sprincean@yahoo.com>
2021-01-05 09:39:29 -0600

    added the functions for the test

--- find-findindex.js
+++ find-findindex.js
@@ -12,7 +12,9 @@ findUserByUsername(users, 'taco') // undefined
 */
 
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find((el) => el.username === username)
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +29,13 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  let idx =  usersArray.findIndex(function(el){
+      return el.username === username;
+  })
+  if (idx === -1){
+      return undefined;
+  } else {
+      return usersArray.splice(idx,1)[0];
+  }
+}
